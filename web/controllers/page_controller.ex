51b845df0af4531387b1661683bd9c06104b8a74
RickR <rrein@akamai.com>
2016-03-15 16:54:29 -0400

    Protect pages controller

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,6 +1,9 @@
 defmodule JanrainGuardian.PageController do
   use JanrainGuardian.Web, :controller
 
+  plug Guardian.Plug.EnsureAuthenticated, handler: JanrainGuardian.Access
+
+
   def index(conn, _params) do
     render conn, "index.html"
   end
